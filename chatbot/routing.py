2b180eda176db0e9f47e0b2bbe2103d2be40897a
kaleem500bc <kaleem_500bc@yahoo.com>
2023-10-08 14:19:02 +0200

    remove unnecessary directories

--- chatbot/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from chatbot.members.GPTResponseConsumer import GPTResponseConsumer
-
-websocket_urlpatterns = [
-    re_path('ws/whisper_gpt_response/', GPTResponseConsumer.as_asgi())
-]
--- chatbot_django/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class History(models.Model):
-    class Meta:
-        app_label = 'chatbot'
-    id = models.AutoField(primary_key=True)
-    user_input = models.TextField(max_length=1000)
-    gpt_response = models.TextField(max_length=1000)
